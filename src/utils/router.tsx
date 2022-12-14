c1b3ac770323eec3c78eed1d4500811afdb16d19
GuVictory <gubanova.vi10@gmail.com>
2022-12-14 18:13:54 +0300

    setup for deploy server

--- src/utils/router.tsx
+++ src/utils/router.tsx
@@ -1,47 +1,44 @@
 import { createBrowserRouter } from 'react-router-dom';
 import { Root, Error, Register, Login, Products, Shop, Product, About, Favorite } from '../pages';
 
-export const router = createBrowserRouter(
-  [
-    {
-      path: '/',
-      element: <Root />,
-      errorElement: <Error />,
-      children: [
-        {
-          path: '',
-          element: <Products />,
-        },
-        {
-          path: 'products',
-          element: <Products />,
-        },
-        {
-          path: 'about',
-          element: <About />,
-        },
-        {
-          path: 'register',
-          element: <Register />,
-        },
-        {
-          path: 'login',
-          element: <Login />,
-        },
-        {
-          path: 'favorite',
-          element: <Favorite />,
-        },
-        {
-          path: 'shop/:shopId',
-          element: <Shop />,
-        },
-        {
-          path: 'product/:productId',
-          element: <Product />,
-        },
-      ],
-    },
-  ],
-  { basename: '/dream_space_web/' },
-);
+export const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Root />,
+    errorElement: <Error />,
+    children: [
+      {
+        path: '',
+        element: <Products />,
+      },
+      {
+        path: 'products',
+        element: <Products />,
+      },
+      {
+        path: 'about',
+        element: <About />,
+      },
+      {
+        path: 'register',
+        element: <Register />,
+      },
+      {
+        path: 'login',
+        element: <Login />,
+      },
+      {
+        path: 'favorite',
+        element: <Favorite />,
+      },
+      {
+        path: 'shop/:shopId',
+        element: <Shop />,
+      },
+      {
+        path: 'product/:productId',
+        element: <Product />,
+      },
+    ],
+  },
+]);
